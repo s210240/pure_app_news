d08bbcb8e55deb6003f69daf487d20d4b13b32bf
roman <s210240@gmail.com>
2023-04-07 10:44:15 +0300

    Update jquery

--- application/views/template_view.php
+++ application/views/template_view.php
@@ -5,7 +5,7 @@
     <title>App news</title>
     <!-- Bootstrap core CSS -->
     <link href="../css/bootstrap.css" rel="stylesheet" crossorigin="anonymous">
-    <script src="../js/jquery-3.4.1.min.js"></script>
+    <script src="../js/jquery-3.6.4.min.js"></script>
 </head>
 <body>
 <div class="container">
